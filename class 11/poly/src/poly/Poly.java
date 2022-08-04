c569e4bfbcdf43aa6ed5eb3f6d32f1d8c20021b0
krishnap2905 <110024793+krishnap2905@users.noreply.github.com>
2022-08-04 11:55:23 +0400

    Add files via upload

---
+++ class 11/poly/src/poly/Poly.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poly;
+
+/**
+ *
+ * @author Hetarth Patel
+ */
+public class Poly {
+    static int add (int a,int b){
+        return a+b;
+    }
+    static double add (double a, double b){
+        return a+b;
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println(add(2, 3));
+    System.out.println(add(3, 4));
+        // TODO code application logic here
+    }
+    
+}
